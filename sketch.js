18e0b22af9faa8c99bc8bae5db7294b715edf60a
Maurus <maurus.amstutz@stud.hslu.ch>
2020-12-03 10:18:01 +0100

    Revert "page transition"
    This reverts commit 4f7ec307506ff7362297a0f80f5f9a298ea5a7a7.

--- sketch.js
+++ sketch.js
@@ -7,43 +7,45 @@ let canvasWidth = 1000
 let canvasHeight = 600
 
 function preload() {
-    img = loadImage('Assetination/Doorbackground.jpg');
-    img2 = loadImage('Assetination/fg.png');
-    img3 = loadImage('Assetination/fg2.png');
-    porchFGImg = loadImage("Assetination/layerfrontporch.png");
-    porchBoxImg = loadImage("Assetination/pinkbox.png");
-    interiorBGImg = loadImage("Assetination/Page1.png");
-    dingimg = loadImage("Assetination/ding.png")
-    dongimg = loadImage("Assetination/dong.png")
-    dotsimg = loadImage("Assetination/dottexture.png")
+  img = loadImage('Assetination/Doorbackground.jpg');
+  img2 = loadImage('Assetination/fg.png');
+  img3 = loadImage('Assetination/fg2.png');
+  porchFGImg = loadImage("Assetination/layerfrontporch.png");
+  porchBoxImg = loadImage("Assetination/pinkbox.png");
+  interiorBGImg = loadImage("Assetination/Page1.png");
+  dingimg = loadImage("Assetination/ding.png")
+  dongimg = loadImage("Assetination/dong.png")
+  dotsimg = loadImage("Assetination/dottexture.png")
 }
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+  createCanvas(canvasWidth,canvasHeight);
 
 
-    // page 1
-    interiorBG = new OurImage(interiorBGImg, 0, 0, 20, 1, 1);
-    ding = new OurImage(dingimg, 50, 50, 20, 0.17, 0.15)
-    dong = new OurImage(dongimg, 80, 140, 20, 0.17, 0.15)
-    dot = new OurImage(dotsimg, 0, 0, 20, 1, 1);
+  // page 1
+  interiorBG = new OurImage(interiorBGImg, 0, 0, 20, 1, 1);
+  ding = new OurImage(dingimg, 50, 50, 20, 0.17, 0.15)
+  dong = new OurImage(dongimg, 80, 140, 20, 0.17, 0.15)
+  dot = new OurImage(dotsimg, 0, 0, 20, 1, 1);
 
 
-    showDingDong = false
-    page1Timer = 0;
+  showDingDong = false
+  page1Timer = 0;
 
-    // page 2
-    image1 = new OurImage(img, 0, 0, 15, 1, 1)
-    frontPorchFG = new OurImage(porchFGImg, 0, 0, 10, 1, 1)
-    frontPorchBox = new OurImage(porchBoxImg, 440, 480, 12, .15, .2)
-    image2 = new OurImage(img2, canvasWidth / 10, canvasHeight / 2, 30, 0.1, 0.2)
-    image3 = new OurImage(img3, canvasWidth / 10, canvasHeight / 2, 30, 0.1, 0.2)
+  // page 2
+  image1 = new OurImage(img, 0, 0, 15, 1, 1)
+  frontPorchFG = new OurImage(porchFGImg, 0, 0, 10, 1, 1)
+  frontPorchBox = new OurImage(porchBoxImg, 440, 480, 12, .15, .2)
+  image2 = new OurImage(img2, canvasWidth / 10, canvasHeight / 2, 30, 0.1, 0.2)
+  image3 = new OurImage(img3, canvasWidth / 10, canvasHeight / 2, 30, 0.1, 0.2)
 }
 
 function draw() {
-    if (page == 1) {
+    if (page == 1)
+    {
         page1draw();
-    } else if (page == 2) {
+    } else if (page == 2)
+    {
         page2draw();
     }
 }
@@ -52,22 +54,18 @@ function page1draw() {
     drawImage(interiorBG);
     drawImage(dot);
 
-    if (interiorBG.IsClicked()) {
-       
-        if (showDingDong) {
-            page = 2;
-            print("aöfjoiewjoö")
-        }
+    if (interiorBG.IsClicked()){
         showDingDong = true;
     }
     if (showDingDong) {
         page1Timer += deltaTime;
         drawImage(ding);
     }
-    if (page1Timer > 400) {
+    if (page1Timer > 400)
+    {
         drawImage(dong);
     }
-}
+} 
 
 function page2draw() {
     drawImage(image1);
@@ -95,16 +93,19 @@ function page2draw() {
 }
 
 
-function drawImage(ourImg) {
-    image(ourImg.img, ourImg.x - ourImg.parallaxOffset + ((mouseX / canvasWidth) * ourImg.parallaxOffset), ourImg.y - ourImg.parallaxOffset + ((mouseY / canvasHeight) * ourImg.parallaxOffset), (canvasWidth + ourImg.parallaxOffset * 2) * ourImg.width, (canvasHeight + ourImg.parallaxOffset * 2) * ourImg.height);
+function drawImage(ourImg)
+{
+    image(ourImg.img, ourImg.x -ourImg.parallaxOffset + ((mouseX / canvasWidth) * ourImg.parallaxOffset), ourImg.y -ourImg.parallaxOffset + ((mouseY / canvasHeight) * ourImg.parallaxOffset), (canvasWidth + ourImg.parallaxOffset*2) * ourImg.width, (canvasHeight + ourImg.parallaxOffset*2) * ourImg.height);
 }
 
 function mousePressed() {
     mouseIsPressed = true;
 }
 
-class OurImage {
-    constructor(img, x, y, parallaxOffset, width, height) {
+class OurImage
+{
+    constructor(img, x, y, parallaxOffset, width, height)
+    {
         this.img = img;
         this.x = x;
         this.y = y;
@@ -113,22 +114,26 @@ class OurImage {
         this.parallaxOffset = parallaxOffset;
     }
 
-    IsClicked() {
+    IsClicked()
+    {
+        print("aöfjoiewjoö")
 
         let circMinX = this.x;
         let circMaxX = this.x + this.width * canvasWidth;
         let circMinY = this.y;
         let circMaxY = this.y + this.height * canvasHeight;
 
-        if (mouseIsPressed && mouseX > circMinX && mouseX < circMaxX && mouseY > circMinY && mouseY < circMaxY) {
+        if (mouseIsPressed && mouseX > circMinX && mouseX < circMaxX && mouseY > circMinY && mouseY < circMaxY)
+        {
             mouseIsPressed = false;
             return true;
         }
 
-        if (mouseIsPressed) {
+        if (mouseIsPressed)
+        {
             mouseIsPressed = false;
         }
-
+        
         return false;
     }
 }
